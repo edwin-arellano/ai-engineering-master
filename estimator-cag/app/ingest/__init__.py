c38726fb39817845424b8b739fdbcbf033ba4657
Edwin Arellano <edwin.i.arellano@gmail.com>
2026-06-04 09:51:16 -0600

    feat(ingest): añade contrato canónico Document/DocumentMetadata

---
+++ estimator-cag/app/ingest/__init__.py
@@ -0,0 +1,6 @@
+"""Subsistema de ingesta de datos (S06).
+
+Paquete autocontenido: implementa los pasos 1-4 del "viaje del dato"
+(catálogo, loaders, parsers, cleaning, normalizers, orquestador) y produce
+``Document`` canónicos en memoria. No se cablea al flujo conversacional/ACB.
+"""
