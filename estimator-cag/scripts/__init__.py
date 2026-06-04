227a8bd1a1a32b911372832dd509d5957bba8888
Edwin Arellano <edwin.i.arellano@gmail.com>
2026-06-04 10:53:23 -0600

    feat(scripts): añade generador de seed sintético reproducible con defectos calibrados

---
+++ estimator-cag/scripts/__init__.py
@@ -0,0 +1,5 @@
+"""Scripts y CLIs del módulo de ingesta (S06).
+
+Ejecutables vía ``uv run python -m scripts.<nombre>``. No forman parte del
+runtime del servicio: generan datos, inspeccionan fuentes, orquestan y demuestran.
+"""
